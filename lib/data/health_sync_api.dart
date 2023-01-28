f15bce5213ddc361a0814ccd60d1886b8b573e22
Davor Komusanac <davor.komusanac@gmail.com>
2023-01-29 00:20:03 +0100

    create data layer with mock values for easier development

---
+++ lib/data/health_sync_api.dart
@@ -0,0 +1,45 @@
+import 'dart:developer';
+
+class HealthSyncApi {
+  //TODO implement Health
+
+  bool isSyncAllowed = false;
+
+  Future<bool> syncRequested() async {
+    try {
+      log('api syncRequested');
+      await Future.delayed(
+        const Duration(seconds: 1),
+      );
+      return isSyncAllowed = true;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<bool> disconnectRequested() async {
+    try {
+      log('api disconnectRequested');
+      await Future.delayed(
+        const Duration(seconds: 1),
+      );
+      return isSyncAllowed = false;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Stream<num> watchStepsAchievedToday() => Stream<num>.periodic(
+        const Duration(seconds: 1),
+        (value) => value + 2,
+      ).takeWhile(
+        (_) => isSyncAllowed,
+      );
+
+  Stream<num> watchCaloriesBurnedToday() => Stream<num>.periodic(
+        const Duration(seconds: 1),
+        (value) => value + 0.5,
+      ).takeWhile(
+        (_) => isSyncAllowed,
+      );
+}
