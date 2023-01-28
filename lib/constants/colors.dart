7cffc9be944cde4710796bad6153b51802aa98d7
Davor Komusanac <davor.komusanac@gmail.com>
2023-01-28 14:13:10 +0100

    load assets, setup icons and colors, setup linter rules

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const orange = Color(0xFFF7A56C);
+  static const fadeGray = Color(0xFFEDF1F3);
+  static const gray = Color(0xFFA6ACB4);
+  static const softBlue = Color(0xFF596274);
+  static const darkBlue = Color(0xFF1F3455);
+}
---
+++ lib/constants/images.dart
@@ -0,0 +1,4 @@
+class ImageAssets {
+  static String flameIcon = 'assets/images/icons/flame.png';
+  static String stepsIcon = 'assets/images/icons/steps.png';
+}
